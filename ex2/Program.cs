ce7b1affcab51c507f3acbeaee44780956687414
drkop <satanatyn@yandex.ru>
2023-03-02 00:22:42 +0300

    2

---
+++ ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine("Введите количество точек у фигуры ");
+int count = int.Parse(Console.ReadLine());
+Console.Write("Введите k: ");
+double k = double.Parse(Console.ReadLine());
+void Answer(double x, double y)
+{  
+    x = k*x; 
+    y = k*y;
+    Console.WriteLine($"({x};{y})");
+}
+for (int i = 0; i < count; i++)
+{
+    Console.Write("Введите x: ");
+    double x = double.Parse(Console.ReadLine());
+    Console.Write("Введите y: ");
+    double y = double.Parse(Console.ReadLine());
+    Console.WriteLine();
+    Answer(x, y);
+}
+
